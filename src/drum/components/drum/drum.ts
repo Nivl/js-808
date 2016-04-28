271ceac01b09341eec3657e5ebbd64c6446f1ecd
Melvin Laplanche <Nivl@users.noreply.github.com>
2016-04-27 22:20:43 -0700

    Add one sequence

--- src/drum/components/drum/drum.ts
+++ src/drum/components/drum/drum.ts
@@ -1,4 +1,5 @@
 import {Component} from 'angular2/core';
+import Sequence from '../../models/sequence';
 
 import './drum.scss';
 
@@ -6,4 +7,112 @@ import './drum.scss';
     selector: 'ml-drum',
     template: require('./drum.html'),
 })
-export default class DrumComponent { }
+export default class DrumComponent {
+  currentSequence: Sequence;
+
+  constructor() {
+    this.useSequenceOne();
+  }
+
+  useSequenceOne() {
+    this.currentSequence = new Sequence();
+    this.currentSequence.setSteps([
+      {
+        kick: { isEnabled: true, },
+        snare: { },
+        hhOpen: { },
+        hhClose: { isEnabled: true, },
+      },
+      {
+        kick: { },
+        snare: { },
+        hhOpen: { },
+        hhClose: { },
+      },
+      {
+        kick: { },
+        snare: { },
+        hhOpen: { isEnabled: true, },
+        hhClose: { },
+      },
+      {
+        kick: { },
+        snare: { },
+        hhOpen: { },
+        hhClose: { },
+      },
+      {
+        kick: { isEnabled: true, },
+        snare: { isEnabled: true, },
+        hhOpen: { },
+        hhClose: { isEnabled: true, },
+      },
+      {
+        kick: { },
+        snare: { },
+        hhOpen: { },
+        hhClose: { },
+      },
+      {
+        kick: { },
+        snare: { },
+        hhOpen: { isEnabled: true, },
+        hhClose: { },
+      },
+      {
+        kick: { },
+        snare: { },
+        hhOpen: { },
+        hhClose: { },
+      },
+      {
+        kick: { isEnabled: true, },
+        snare: { },
+        hhOpen: { },
+        hhClose: { isEnabled: true, },
+      },
+      {
+        kick: { },
+        snare: { },
+        hhOpen: { },
+        hhClose: { },
+      },
+      {
+        kick: { },
+        snare: { },
+        hhOpen: { isEnabled: true, },
+        hhClose: { },
+      },
+      {
+        kick: { },
+        snare: { },
+        hhOpen: { },
+        hhClose: { },
+      },
+      {
+        kick: { isEnabled: true, },
+        snare: { isEnabled: true, },
+        hhOpen: { },
+        hhClose: { isEnabled: true, },
+      },
+      {
+        kick: { },
+        snare: { },
+        hhOpen: { },
+        hhClose: { },
+      },
+      {
+        kick: { },
+        snare: { },
+        hhOpen: { isEnabled: true, },
+        hhClose: { },
+      },
+      {
+        kick: { },
+        snare: { },
+        hhOpen: { },
+        hhClose: { },
+      }
+    ]);
+  }
+}
