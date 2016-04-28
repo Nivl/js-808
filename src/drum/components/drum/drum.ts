431a87b8b55119067b149d1782448e70998b4753
Melvin Laplanche <Nivl@users.noreply.github.com>
2016-04-28 02:14:46 -0700

    Add playback

--- src/drum/components/drum/drum.ts
+++ src/drum/components/drum/drum.ts
@@ -12,9 +12,42 @@ import './drum.scss';
 })
 export default class DrumComponent {
   currentSequence: Sequence;
+  isPlaying: boolean = false;
 
   constructor() {
     this.useSequenceOne();
+  }
+
+  stop() {
+    this.isPlaying = false;
+  }
+
+  play() {
+    if (this.isPlaying === false) {
+      if (this.currentSequence.steps.length === 0 || this.currentSequence.steps.length % 4) {
+        console.error('Sequence not valid');
+        return ;
+      }
+
+      console.log('start');
+      this.isPlaying = true;
+      this.playNext(0);
+    }
+  }
+
+  private playNext(i) {
+    if (this.isPlaying) {
+      console.log(this.currentSequence.bpm, typeof this.currentSequence.bpm);
+
+      const waitTime = ((60 / ~~this.currentSequence.bpm) / 4) * 1000;
+      this.currentSequence.steps[i].isPlaying = true;
+
+      setTimeout(() => {
+        const nextIndex = (i + 1 === this.currentSequence.steps.length) ? (0) : (i + 1);
+        this.currentSequence.steps[i].isPlaying = false;
+        this.playNext(nextIndex);
+      }, waitTime);
+    }
   }
 
   useSequenceOne() {
