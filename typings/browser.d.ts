ee8e0dadf68a3c76c329b04235ee5347edd433ad
Melvin Laplanche <Nivl@users.noreply.github.com>
2016-04-27 18:33:56 -0700

    Add howler

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="browser/ambient/es6-shim/index.d.ts" />
+/// <reference path="browser/ambient/howler/index.d.ts" />
 /// <reference path="browser/ambient/jasmine/index.d.ts" />
 /// <reference path="browser/ambient/require/index.d.ts" />
