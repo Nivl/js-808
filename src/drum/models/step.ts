fd00ddcc05a9dbdd0fdd2699efab597dbd822c15
Melvin Laplanche <Nivl@users.noreply.github.com>
2016-04-29 18:50:04 -0700

    Set the arg of the step constructor as optional

--- src/drum/models/step.ts
+++ src/drum/models/step.ts
@@ -2,10 +2,10 @@ import {Kick, Snare, HhOpen, HhClose} from './instruments';
 import {INoteOptions} from './inote';
 
 export interface IStepOptions {
-  kick: INoteOptions;
-  snare: INoteOptions;
-  hhOpen: INoteOptions;
-  hhClose: INoteOptions;
+  kick?: INoteOptions;
+  snare?: INoteOptions;
+  hhOpen?: INoteOptions;
+  hhClose?: INoteOptions;
 }
 
 export class Step {
@@ -15,11 +15,11 @@ export class Step {
   hhOpen: HhOpen;
   hhClose: HhClose;
 
-  constructor(data: IStepOptions) {
-    this.kick = new Kick(data.kick);
-    this.snare = new Snare(data.snare);
-    this.hhOpen = new HhOpen(data.hhOpen);
-    this.hhClose = new HhClose(data.hhClose);
+  constructor(data: IStepOptions = {}) {
+    this.kick = new Kick(data.kick || {});
+    this.snare = new Snare(data.snare || {});
+    this.hhOpen = new HhOpen(data.hhOpen || {});
+    this.hhClose = new HhClose(data.hhClose || {});
   }
 
   play() {
